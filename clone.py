7951576efb937869005d47ad3d4e20896c229627
fcoulombe <fcoulombe@silentfalls.org>
2013-07-17 02:04:50 -0300

    python script to clone the 3rdparties

---
+++ clone.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+	os.system("git clone  https://github.com/fcoulombe/OpenCOLLADA.git")
+	os.system("git clone  https://github.com/madler/zlib.git")
+	os.system("git clone  https://github.com/fcoulombe/eglew.git")
+	os.system("git clone  https://github.com/fcoulombe/freetype2.git")
+	os.system("git clone  https://github.com/fcoulombe/libpng.git")
+	os.system("git clone  https://github.com/fcoulombe/ogg.git")
+	os.system("git clone  https://github.com/fcoulombe/vorbis.git")
+	os.system("git clone  git://repo.or.cz/openal-soft.git openal")
+	
+
+if __name__ == "__main__":
+	main()
