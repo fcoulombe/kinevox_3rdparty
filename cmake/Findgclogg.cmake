777ee3c9021064374b5a01f03d29591738568d11
fcoulombe <fcoulombe@silentfalls.org>
2014-05-11 19:24:27 -0300

    fixing the library name so that it works on windows and linux

--- cmake/Findgclogg.cmake
+++ cmake/Findgclogg.cmake
@@ -2,8 +2,11 @@ SET(DepName "gclogg")
 
 SET(OGG_ROOT "${CMAKE_CURRENT_LIST_DIR}/../binaries/${CMAKE_SYSTEM_NAME}${CMAKE_CXX_COMPILER_ID}${KINEVOX_ARCHITECTURE}/usr/local/")
 SET(LPATH ${OGG_ROOT}/lib/)
-SET(LIB_NAME "ogg")
-
+if(${WIN32})
+    SET(LIB_NAME "ogg")
+else()
+    SET(LIB_NAME "libogg")
+endif()
 	
 SET(OGG_INCLUDE_DIR ${OGG_ROOT}/include)
 FindLibrary(OGG_LIBRARY ${LIB_NAME} ${LPATH})
